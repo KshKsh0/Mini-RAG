4388aafc86d9e33ca8c14d04017986235a4124d6
Ahmed <kakashialhmouz@gmail.com>
2026-02-19 16:13:40 +0300

    enums for the llm we will use

---
+++ src/stores/LLM/LLMEmums.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class LLMEnum(Enum):
+    OPENAI = 'OPENAI'
+    COHERE = 'COHERE'
+
+class OpenAIEnum(Enum):
+    SYSTEM ='system'
+    USER = 'user'
+    ASSISTANT ='assistant'
+
+class CoHereEnums(Enum):
+    SYSTEM = 'system'
+    USER = 'user'
+    ASSISTANT= 'CHATBOT'
+    DOCUMENT ='search_document'
+    QUERY = 'search_query'
+
+
+class DocumentTypeEnum(Enum):
+    DOCUMENT = 'document'
+    QUERY = 'query'
